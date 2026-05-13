e0b9f19711993b5a53c0a09493843d0b247a0154
unknown <anisabekteshi4@gmail.com>
2026-05-13 19:14:49 +0200

    working on register

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,59 @@ import React from 'react'
 import { NavLink, Link } from "react-router-dom";
 
 function Navbar() {
+    const user = null;
+      const activeLink = ({ isActive }) =>
+        isActive
+            ? "bg-blue-600 text-white px-3 py-1 rounded"
+            : "px-3 py-1 hover:text-gray-600"; 
+
     return (
         <header>
-            <nav>
-                <Link to="/">Real Estate</Link>
+            <nav className='px-4 py-6 shadow-sm sticky top-0 z-50'>
+               <div className='flex items-center justify-between max-w-6xl mx-auto'>
+                 <Link to="/" className='text-2xl text-blue-900 font-bold'> <span className='text-blue-600'>Real</span>Estate</Link>
+               
 
-                <ul className='flex items-center'>
-                    <NavLink to="/">Home  </NavLink>
-                    <NavLink to="/login">Log in  </NavLink>
-                    <NavLink to="/register">Register  </NavLink>
-                    <NavLink to="/addproperty">Add property  </NavLink>
-                    <NavLink to="/dashboard">Dashboard  </NavLink>
-                    <NavLink to="/myproperties">My properties  </NavLink>
-                    <NavLink to="/properties">Properties  </NavLink>
+                <ul className='flex justify-between gap-6'>
+                    <li> 
+                        <NavLink to="/" className={activeLink}>Home  </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/properties" className={activeLink}>Properties  </NavLink>
+                    </li>
+                    {!user &&(
+                        <>
+                            <li>
+                        <NavLink to="/login" className={activeLink}>Log in  </NavLink>
+                    </li>
+                     <li>
+                        <NavLink to="/register" className={`${activeLink} bg-blue-500 text-blue-50 p-2 rounded-sm`} >Register  </NavLink>
+                     </li>
+                        </>
+                    )}
+
+                    {user &&(
+                        <>
+                             <li>
+                        <NavLink to="/dashboard" className={activeLink}>Dashboard  </NavLink>
+                     </li>
+
+                     <li>
+                        <NavLink to="/myproperties" className={activeLink}>My properties  </NavLink>
+                     </li>
+
+                     <li>
+                        <NavLink to="/addproperty" className={activeLink}>Add property  </NavLink>
+                     </li>
+
+                    <li>
+                        <NavLink to="/" className={activeLink}>Log Out</NavLink>
+                    </li>
+                        </>
+                    )
+                    }
                 </ul>
+                </div>
             </nav>
         </header>
     )
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { registerUser } from '../services/auth';
 
 function Register() {
+
+  const navigate = useNavigate();
+  const [fullName,setFullName]= useState("");
+  const [error,setError]=useState("");
+  
+
+    const handleSubmit = (e)=>{
+      e.preventDefault();
+      if (!fullName){
+        setError("please all fields");
+        return
+      }
+      registerUser({fullName}).then((result)=>{
+        console.log(result);
+        navigate("/login")
+      })
+    }
   return (
-    <div>Register</div>
+    <main className='py-12 px-4'>
+      <div className='max-w-md mx-auto'>
+         <h1 className='text-2xl text-center mb-1'>Create an acount</h1>
+         <form onSubmit={handleSubmit} className='rounded-xl shadow-2xl border border-gray-200 space-y-5 p-5'>
+            {error &&
+            <p className='rounded-lg bg-red-300 text-red-700'>{error}</p>}
+            <label htmlFor="">
+              <span className='block text-sm font-medium text-gray-700 mb-1'>Full Name</span>
+              <input 
+              value={fullName}
+              onChange={(e)=>{setFullName(e.target.value)}}
+              type="text" className='w-full rounded-lg border border-gray-300 py-2' />
+            </label>
+
+
+              <button type='submit' className=" w-full mt-2 px-4 py-2 bg-blue-600 text-white font-medium rounded-lg">Register</button>
+            
+         </form>
+      </div>
+    </main>
   )
 }
 
